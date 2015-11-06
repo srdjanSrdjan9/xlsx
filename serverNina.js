7de05f2625150d2404a60480f639fc6385c9ce92
ninam777 <nikolina-nina@live.com>
2015-11-06 17:58:22 +0100

    Novo
    ne gledaj ovo :D

---
+++ serverNina.js
@@ -0,0 +1,27 @@
+//Lets require/import the HTTP module
+var http = require('http');
+var dispatcher = require('httpdispatcher');
+
+//Lets define a port we want to listen to
+const PORT=8080; 
+
+//We need a function which handles requests and send response
+function handleRequest(request, response){
+	try{
+		console.log(request.url);
+
+		dispatcher.dispatch(request,response);
+	}catch(err){
+		console.log(err);
+	}
+    //response.end('It Works!! Path Hit: ' + request.url);
+}
+
+//Create a server
+var server = http.createServer(handleRequest);
+
+//Lets start our server
+server.listen(PORT, function(){
+    //Callback triggered when server is successfully listening. Hurray!
+    console.log("Server listening on: http://localhost:%s", PORT);
+});
